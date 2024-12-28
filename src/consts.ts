9223883c3fe866fb96badc6bd8871a76cb22a87a
Chenyang <cgartlab@outlook.com>
2024-12-29 04:21:59 +0800

    init all

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'CGArtLab';
+export const SITE_DESCRIPTION = '数字艺术/视觉设计/技术分享/知识管理';
