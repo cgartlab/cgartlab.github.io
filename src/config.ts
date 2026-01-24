6e93d653e8d0b5f2c25476c9752737b59815cdf2
cgartlab <cgartlab@outlook.com>
2026-01-24 22:11:10 +0800

    关闭 waline 评论模块

--- src/config.ts
+++ src/config.ts
@@ -102,7 +102,7 @@ export const themeConfig: ThemeConfig = {
     // https://waline.js.org/en/
     waline: {
       // server url
-      serverURL: 'https://retypeset-comment.radishzz.cc',
+      // serverURL: 'https://retypeset-comment.radishzz.cc',
       // emoji url
       emoji: [
         'https://unpkg.com/@waline/emojis@1.2.0/tw-emoji',
